aae63878655e3adf036a3619f6d7153571f76f8b
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-11 10:51:07 -0400

    Final Commit

--- src/MadLib.java
+++ src/MadLib.java
@@ -4,6 +4,30 @@
  */
 
 
-public class MadLib {
+import java.util.Scanner;
 
+public class MadLib {
+    public static void main(String[] args){
+
+        System.out.print("Enter a noun: ");
+        Scanner nounInp = new Scanner(System.in);
+        String noun = nounInp.nextLine();
+
+        System.out.print("Enter a verb: ");
+        Scanner verbInp = new Scanner(System.in);
+        String verb = verbInp.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        Scanner adjInp = new Scanner(System.in);
+        String adj = adjInp.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        Scanner adverbInp = new Scanner(System.in);
+        String adverb = adverbInp.nextLine();
+
+        System.out.printf("Do you %s your %s %s %s? That's hilarious!",verb,adj,noun,adverb);
+
+
+
+    }
 }
